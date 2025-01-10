5efeaef8574492aa40670e171000fe219b61d077
Oleh Mushka <olegamysk@gmail.com>
2025-01-10 20:32:18 +0200

    Refactor utils.ts

--- lib-ts/utils/utils.ts
+++ lib-ts/utils/utils.ts
@@ -1,3 +1,19 @@
-import * as _ from 'lodash';
+The code you provided imports the entire lodash library and exports it as the default export. While this is functional, it's not the most efficient way to use lodash, especially if you're only using a few functions from the library. Importing the entire library can increase the bundle size unnecessarily.
 
-export default _;
+Here's a more efficient way to handle lodash imports:
+
+1. Import only the specific functions you need from lodash. This can help reduce the bundle size if you're using a bundler that supports tree-shaking, like Webpack.
+
+2. If you need to export lodash as a whole, consider whether you really need to do this. It's often better to import and use only what you need in each module.
+
+Here's an example of how you might improve the code by importing specific functions:
+
+```typescript
+// Import specific functions from lodash
+import { map, filter } from 'lodash';
+
+// Export the functions you need
+export { map, filter };
+```
+
+If you truly need to export the entire lodash library, and you have a good reason for doing so, you can keep your original code. However, be mindful of the potential impact on your application's bundle size.
