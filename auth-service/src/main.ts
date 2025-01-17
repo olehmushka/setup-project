1d9dd8aa73e85c93e80b6d942eb14f4e9bb3288f
Oleh Mushka <olegamysk@gmail.com>
2025-01-17 21:50:18 +0200

    Refactor: Improve auth-service/src/main.ts

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -2,9 +2,11 @@ import Debug from 'debug';
 import { app } from './app';
 
 const debug = Debug('http');
+const port = app.get('port');
+const env = app.get('env');
 
-app.listen(app.get('port'), () => {
-  debug(`App is running at http://localhost:${app.get('port')} in ${app.get('env')} mode`);
+app.listen(port, () => {
+  debug(`App is running at http://localhost:${port} in ${env} mode`);
   debug('Press CTRL-C to stop');
 });
 
